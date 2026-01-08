b264a2406cdd46fa6dfbf540693bb872d5cf2cdc
Andre Krautschick <andre.krautschick@eurocontrol.int>
2026-01-08 14:34:48 +0000

    RF-28 updated trap reception

--- src/SnmpLibrary/traps.py
+++ src/SnmpLibrary/traps.py
@@ -17,6 +17,7 @@ import warnings
 import functools
 
 import robot.utils
+from robot.api import logger
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -30,16 +31,20 @@ from . import utils
 
 
 def _generic_trap_filter(domain, sock, pdu, **kwargs):
+    logger.trace("Filter parameter: %s" % kwargs)
+    logger.trace("Received PDU:\n%s" % pdu)
     snmpTrapOID = (1, 3, 6, 1, 6, 3, 1, 1, 4, 1, 0)
     if 'host' in kwargs and kwargs['host']:
         if sock[0] != kwargs['host']:
             return False
 
-    for oid, val in v2c.apiPDU.getVarBindList(pdu):
+    for oid, val in v2c.apiPDU.get_varbinds(pdu):
         if 'oid' in kwargs and kwargs['oid']:
             if oid == snmpTrapOID:
-                if val[0][0][2] != v2c.ObjectIdentifier(kwargs['oid']):
+                if val != v2c.ObjectIdentifier(kwargs['oid']):
+                    logger.info("Expected trap not received!")
                     return False
+    logger.info("Expected trap received!")
     return True
 
 
@@ -60,30 +65,39 @@ def _trap_receiver(trap_filter, host, port, timeout):
             raise RuntimeError('Only SNMP v2c traps are supported.')
 
         req, msg = decoder.decode(msg, asn1Spec=v2c.Message())
-        pdu = v2c.apiMessage.getPDU(req)
+        pdu = v2c.apiMessage.get_pdu(req)
 
-        # ignore any non trap PDUs
+    # ignore any non trap PDUs
         if not pdu.isSameTypeWith(v2c.TrapPDU()):
             return
 
         # Stop the receiver if the trap we are looking for was received.
         if trap_filter(domain, sock, pdu):
-            transport.jobFinished(1)
+            transport.job_finished(1)
+            transport._AsyncioDispatcher__close_dispatcher()
 
     dispatcher = AsyncioDispatcher()
-    dispatcher.registerRecvCbFun(_trap_receiver_cb)
-    dispatcher.registerTimerCbFun(_trap_timer_cb)
+    dispatcher.register_recv_callback(_trap_receiver_cb)
+    # Timer call back doesn't pass Exception (see below).
+    # It's not working as expected and therefore currently not used.
+    #dispatcher.register_timer_callback(_trap_timer_cb)
 
-    transport = udp.UdpSocketTransport().openServerMode((host, port))
-    dispatcher.registerTransport(udp.domainName, transport)
+    transport = udp.UdpAsyncioTransport().open_server_mode((host, port))
+    dispatcher.register_transport(udp.DOMAIN_NAME, transport)
 
+    dispatcher.job_started(1)
+    dispatcher.run_dispatcher(timeout)
+
+    # Following code is not working as expected. Currently it's not clear why this is happening.
+    # Raised exception is not caught. In case the issue is found, it will be re-implemented.
     # we'll never finish, except through an exception
-    dispatcher.jobStarted(1)
-
-    try:
-        dispatcher.runDispatcher()
-    finally:
-        dispatcher.closeDispatcher()
+    # try:
+    #     dispatcher.run_dispatcher()
+    # finally:
+    #     # Not using public method as described in next line.
+    #     # dispatcher.close_dispatcher()
+    #     # This is due to issue of pysnmp lib reported here: https://github.com/lextudio/pysnmp/issues/203
+    #     dispatcher._AsyncioDispatcher__close_dispatcher()
 
 
 class _Traps:
